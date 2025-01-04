59e632abe888c3cf258baf8278f8778e1bd09378
Mayday46 <winstonnlinn@gmail.com>
2025-01-03 22:21:13 -0500

    Initialize files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./style.css";
+
+
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
